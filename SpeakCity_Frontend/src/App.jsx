f305c32762041c8f6394de55f40815106d64e725
Uriel Sánchez Sánchez <166778132+UrielSS@users.noreply.github.com>
2025-06-08 16:43:40 -0600

    Se agrega componente Chatbox al App

--- SpeakCity_Frontend/src/App.jsx
+++ SpeakCity_Frontend/src/App.jsx
@@ -22,7 +22,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
+       <h1>SpeakCity - IA Chat</h1>
+      
+      <ChatBox />
       <div className="card">
         <h2>
           <ul>
