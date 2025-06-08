20b2fd77314d59bb3e744a01a9bad385d25c6787
Uriel Sánchez Sánchez <166778132+UrielSS@users.noreply.github.com>
2025-06-08 16:51:54 -0600

    Corrección de Import

--- SpeakCity_Frontend/src/App.jsx
+++ SpeakCity_Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ChatBox from './ChatBox'
 
 function App() {
   const [users, setUsers] = useState([])
